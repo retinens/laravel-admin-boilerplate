741bd88f60bbe33257f884781d8e412f0f9f8e47
Lucas Houssa <lucas@retinens.be>
2020-02-29 14:27:47 +0100

    update

--- routes/admin.php
+++ routes/admin.php
@@ -4,5 +4,5 @@
 Route::group(['as' => 'admin.','prefix' => 'admin','namespace' => 'Admin','middleware' => ['auth','is_admin']], function (){
     Route::get('/',function (){
         return view('admin.pages.home');
-    })->name('home');
+    })->name('index');
 });
